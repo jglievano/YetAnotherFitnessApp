49ca97d32725c403e2b76c02f759dc6a4df19599
Gabriel Lievano <jg@newpixeltheory.com>
2019-02-23 16:12:02 -0800

    Makes the project runnable.
    Removes main storyboard requirement and adds the
    LaunchScreen.storyboard.

--- ios-app/Classes/AppDelegate.swift
+++ ios-app/Classes/AppDelegate.swift
@@ -1,28 +1,23 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate:
-    UIResponder,
-    UIApplicationDelegate {
-
+class AppDelegate: UIResponder {
   var window: UIWindow?
+  
+  func afterLaunchSetup() {}
+}
 
+extension AppDelegate: UIApplicationDelegate {
   func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
+    ) -> Bool {
+    afterLaunchSetup()
     return true
   }
-
   func applicationWillResignActive(_ application: UIApplication) {}
-
   func applicationDidEnterBackground(_ application: UIApplication) {}
-
   func applicationWillEnterForeground(_ application: UIApplication) {}
-
   func applicationDidBecomeActive(_ application: UIApplication) {}
-
   func applicationWillTerminate(_ application: UIApplication) {}
 }
-
-
